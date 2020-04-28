88990d3229f3e6ef280f3b76176d652ad3c34c04
Ayrton Felipe <tunnes@MacBook-Pro-de-Ayrton.local>
2020-04-28 02:07:22 -0300

    Adjust timezone

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module MonkeySchedulerBot
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+     config.time_zone = "America/Sao_Paulo"
   end
 end
